466bc64ca3102d1b9b992e6674b6ddc3578f88ba
Roger Sundh <roger.sundh@musiktronik.com>
2015-03-21 13:49:50 +0100

    Test

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="sv">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="UTF-8" />
+        <title>Guess numbers</title>
     </head>
     <body>
         <ul>
